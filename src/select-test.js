b5edd9e6f8cbbfbe42a09996d751ac05f78677a9
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-06-18 15:52:37 +0300

    add custom select my-library

--- src/select-test.js
+++
@@ -1,8 +0,0 @@
-const selectEl = document.querySelector('.select');
-const optionEl = document.querySelector('.options');
-
-selectEl.addEventListener('click', onSelectClick);
-
-function onSelectClick(e) {
-  optionEl.classList.toggle('option-show');
-}

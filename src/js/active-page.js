944995d01c9feeb02d788f4dc4ba078c24ddc96e
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-07-24 23:14:49 +0300

    Update active-page.js

--- src/js/active-page.js
+++ src/js/active-page.js
@@ -1,13 +1,15 @@
 const pagesEl = document.querySelectorAll('.js-header-nav-link');
 
 const windowPathName = window.location.pathname;
-console.log('windowPathName:', windowPathName);
 
 pagesEl.forEach(pageEl => {
   const pageLinkPathname = new URL(pageEl.href).pathname;
 
-  //? Тут потрібно написати імя репозиторію
-  if (windowPathName === '/cinemania' && pageLinkPathname === '/index.html')
+  //? Тут потрібно написати імя репозиторію, щоб при першому візиті додався клас
+  if (
+    windowPathName === '/cinemania/' &&
+    pageLinkPathname === '/cinemania/index.html'
+  )
     pageEl.classList.add('active');
 
   if (pageLinkPathname === windowPathName) pageEl.classList.add('active');

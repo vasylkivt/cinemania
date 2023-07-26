d9981fb306d238f1795fa708b0e89cdc30f01607
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-07-26 19:53:18 +0300

    add modal style

--- src/js/components/modal-movie-card.js
+++ src/js/components/modal-movie-card.js
@@ -1,8 +1,5 @@
 import comingSoonImg from '../../images/default_horizontal_poster_path.jpg';
-
-const svgCloseIcon = `<svg width="10" height="10" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path class="svg-close-icon" d="M11.25 11.25L0.75 0.75M11.25 0.75L0.75 11.25" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`;
+import svgCloseIcon from '../../images/icon/sprite.svg';
 
 export const markupModalMovieCard = ({
   poster_path,
@@ -14,36 +11,43 @@ export const markupModalMovieCard = ({
   overview,
 }) => {
   return `
+ 
+  <div class="modal-movie-card-wrap"> 
+  <div>
+    <button type="button" class="js-close-modal-btn modal-movie-card-button-close">
+      <svg class="modal-movie-card-button-close-icon">
+        <use href="${svgCloseIcon}#icon-button-close"></use>
+      </svg>
+    </button>
     <img
-        class="modal-movie-card-img"
-        loading="lazy"
-        width="280"
-        height="402"
-        ${getImg(poster_path, original_title)}
-      />
+      class="modal-movie-card-img"
+      loading="lazy"
+      width="280"
+      height="402"
+      ${getImg(poster_path, original_title)}
+    />
+  </div>
+  <div>
+    <h3 class="modal-movie-card-title">${original_title}</h3>
+    <div class="modal-movie-card-details-wrapper">
+      <p class="modal-movie-card-details-names">Vote / Votes</p>
+      <p class="modal-movie-card-detail-vote">
+        <span>${vote_average.toFixed(1)}</span> / <span>${vote_count}</span>
+      </p>
+      <p class="modal-movie-card-details-names">Popularity</p>
+      <p class="modal-movie-card-detail-popularity">${popularity}</p>
+      <p class="modal-movie-card-details-names">Genre</p>
+      <p class="modal-movie-card-detail-genre">${allGenres(genres)}</p>
+    </div>
+    <p class="modal-movie-card-desc-title">About</p>
+    <p class="modal-movie-card-desc">${overview}</p>
 
-      <div>
-        <h3 class="modal-movie-card-title">${original_title}</h3>
-        <div class="modal-movie-card-details-wrapper">
-        
-         
-          <p class="modal-movie-card-details-names">Vote / Votes</p>
-          <p class="modal-movie-card-detail-vote">
-            <span>${vote_average.toFixed(1)}</span> / <span>${vote_count}</span>
-          </p>
-          <p class="modal-movie-card-details-names">Popularity</p>
-          <p class="modal-movie-card-detail-popularity">${popularity}</p>
-          <p class="modal-movie-card-details-names">Genre</p>
-          <p class="modal-movie-card-detail-genre">${allGenres(genres)}
-          </p>
-        </div>
-        <p class="modal-movie-card-desc-title">About</p>
-        <p class="modal-movie-card-desc">
-          ${overview}
-        </p>
-
-        <button class="js-add-remove-btn button-accent">Add to my library</button> <button class="js-close button-accent">CLOSE</button>
-      </div>`;
+    <button type="button" class="js-add-remove-btn button-accent">
+      Add to my library
+    </button>
+  </div>
+</div>
+  `;
 };
 
 function allGenres(genres) {
